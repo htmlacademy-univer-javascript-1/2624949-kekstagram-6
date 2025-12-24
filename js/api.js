3ff6e8fb38007504fcc28f3f2d560453a0f07e1c
finnesoff <EA.Kovrizhnykh@urfu.me>
2025-12-24 09:15:19 +0300

    11.9. Надо подкачаться

---
+++ js/api.js
@@ -0,0 +1,24 @@
+const BASE_URL = 'https://29.javascript.htmlacademy.pro/kekstagram';
+
+const getData = () =>
+  fetch(`${BASE_URL}/data`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Не удалось загрузить данные');
+      }
+      return response.json();
+    });
+
+const sendData = (formData) =>
+  fetch(BASE_URL, {
+    method: 'POST',
+    body: formData,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Не удалось отправить форму');
+      }
+      return response.json();
+    });
+
+export { getData, sendData };
