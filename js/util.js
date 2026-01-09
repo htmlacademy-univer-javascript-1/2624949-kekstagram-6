2a01d4fe897dec6956a60c3fefc74686d486461a
finnesoff <darya_katya@mail.ru>
2026-01-09 17:16:06 +0500

    Д5

--- js/util.js
+++ js/util.js
@@ -15,24 +15,11 @@ export const showDataLoadError = (message) => {
   document.body.append(errorMessage);
 };
 
-export function debounce(callback, timeoutDelay = 500) {
+export const debounce = (callback, timeoutDelay = 500) => {
   let timeoutId;
 
   return (...rest) => {
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
   };
-}
-
-export function throttle(callback, delayBetweenFrames) {
-  let lastTime = 0;
-
-  return (...rest) => {
-    const now = new Date();
-
-    if (now - lastTime >= delayBetweenFrames) {
-      callback.apply(this, rest);
-      lastTime = now;
-    }
-  };
-}
+};
